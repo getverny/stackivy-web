d04a8a558283690958cfe83f6ab60a52a50f8afc
orlamee <meetmichaelakindele@gmail.com>
2023-04-18 19:56:52 +0100

    stack

---
+++ src/components/unlock.jsx
@@ -0,0 +1,14 @@
+
+function Unlocked() {
+  return (
+    <section className="unlocked bg-white">
+      <div className="container">
+        <div className="row">
+          div
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Unlocked
