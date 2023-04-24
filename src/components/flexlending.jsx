948a1fbee3b56ce7575b6a9858ae27f82cae3046
orlamee <meetmichaelakindele@gmail.com>
2023-04-24 08:26:41 +0100

    fix

--- src/components/flexlending.jsx
+++ src/components/flexlending.jsx
@@ -6,7 +6,7 @@ function FlexLending() {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-sm-8 text-center">
-            <h1>Flex <span className="lend">Lending</span></h1>
+            <h1>Flexible <span className="lend">Lending</span></h1>
             <p style={{marginTop: "34px" , marginBottom: "75px"}}>At Hargon, we recognize that every business or personal financial situation is unique. That is why we offer a wide range of flexible lending solutions to match your specific need as close as possible. Whether it is a long-term loan, short-term loan or buy now, pay later type of situation, Hargon has got you covered.</p>
             <button
               className="btn btn-primary btn-grad rounded-pill px-5 py-4"
