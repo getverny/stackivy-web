be0913a8a328ad223d0159c71b4b6e96f2ac4138
orlamee <meetmichaelakindele@gmail.com>
2023-04-25 16:58:10 +0100

    fixes

--- src/components/budgetingHero.jsx
+++ src/components/budgetingHero.jsx
@@ -11,7 +11,13 @@ function BudgetingHero() {
         </div>
         <div className="row justify-content-center mt-5">
           <div className="col-sm-7 text-center">
-            <h6 className="">Tracking and controlling your finances plays a significant role in wealth building. However, it can be quite challenging, especially when there is a lot you need to do with your money. Fortunately, at Stackivy, we have created a couple of budgeting tools to help you easily manage your finances. Our user-friendly budgeting tools, infused with advanced features, make tracking, controlling, and achieving goals much more fun.</h6>
+            <h6 className="mb-5">Tracking and controlling your finances plays a significant role in wealth building. However, it can be quite challenging, especially when there is a lot you need to do with your money. Fortunately, at Stackivy, we have created a couple of budgeting tools to help you easily manage your finances. Our user-friendly budgeting tools, infused with advanced features, make tracking, controlling, and achieving goals much more fun.</h6>
+            <button
+              className="btn btn-primary btn-green rounded-pill px-5 py-4 mt-5"
+              to="" style={{background: 'linear-gradient(117.04deg, #2563EB -13.79%, #35FF49 131.52%)'}}
+            >
+              Get started
+            </button>
           </div>
         </div>
       </div>
