4b645f228d744f92cc50501dcb93fd57138f8173
orlamee <meetmichaelakindele@gmail.com>
2023-04-27 10:26:54 +0100

    Education

--- src/pages/financialEducation.jsx
+++ src/pages/financialEducation.jsx
@@ -1,8 +1,11 @@
+import BePart from "../components/bePart";
+import Best from "../components/bestDecision";
 import EducationResources from "../components/educationalResources";
 import FinHeader from "../components/finHeader";
 import FinHero from "../components/finHero";
 import FinancialInfo from "../components/financialInfo";
 import NoTop from "../components/footerwithoutTop";
+import KeedZone from "../components/keedZone";
 
 
 function FinancialEducation() {
@@ -11,6 +14,9 @@ function FinancialEducation() {
       <FinHeader/>
       <FinHero/>
       <EducationResources/>
+      <KeedZone/>
+      <Best/>
+      <BePart/>
       <FinancialInfo/>
       <NoTop/>
     </div>
