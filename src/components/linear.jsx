fe3b330475f76d05e7927aa2b2fa41aca6382d6a
orlamee <meetmichaelakindele@gmail.com>
2023-04-27 15:07:55 +0100

    fixes

--- src/components/linear.jsx
+++ src/components/linear.jsx
@@ -1,16 +1,17 @@
+import why from "../assets/man.svg";
 
 function Linear() {
   return (
     <section className="linear">
       <div className="container">
         <div className="row justify-content-between">
-         <div className="col-sm-8">
-          <h3 className="text-white mb-4">Why our <span style={{fontWeight: "600"}}>Personal Wealth management </span>matters</h3>
-          <p className="text-white">Our wealth management platform allows you</p>
-          <div className="toppings">
-            <span className="text-white carou">Build sustainable wealth</span>
+          <div className="col-sm-8">
+            <img src={why} alt="why" className="img-fluid mb-4" />
+            <p className="text-white">Our wealth management platform allows you</p>
+            <div className="toppings">
+              <span className="text-white carou">Build sustainable wealth</span>
+            </div>
           </div>
-         </div>
         </div>
       </div>
     </section>
