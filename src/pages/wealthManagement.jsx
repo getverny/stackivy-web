f393d59c78cfd1b405c25113320f4f936e45e022
orlamee <meetmichaelakindele@gmail.com>
2023-04-27 11:40:35 +0100

    fix

--- src/pages/wealthManagement.jsx
+++ src/pages/wealthManagement.jsx
@@ -1,10 +1,12 @@
 import BudgetFooter from "../components/budgetFooter";
+import Info from "../components/info";
 import Header from "../components/navBar";
 
 function Wealth() {
   return (
     <div>
       <Header/>
+      <Info/>
       <BudgetFooter/>
     </div>
   );
