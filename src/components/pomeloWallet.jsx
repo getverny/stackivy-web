8097ce0719fefe585df827842dcd4b7961f5c532
orlamee <meetmichaelakindele@gmail.com>
2023-05-03 11:45:58 +0100

    pomelo

--- src/components/pomeloWallet.jsx
+++ src/components/pomeloWallet.jsx
@@ -1,15 +1,21 @@
-import barcode from "../assets/globals.svg";
-import dev from "../assets/pomelodev.svg";
+import per from "../assets/businez.svg";
+import gql from "../assets/gql.svg";
 
 
 function PomeloWallet() {
   return (
-    <section className="pomelowallet bg-white">
+    <section className="pomelowallet bg-white pb-0">
       <div className="container">
-        <div className="row justify-content-between qrcode">
-          <div className="col-sm-5 mb-3">
-            <h3 className="mb-4"><span className="pr">Pomelo</span>Wallet</h3>
-            {/* <img src={barcode} alt="barcode" className="img-fluid w-100" /> */}
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={per} alt="personal" className="img-fluid" />
+          </div>
+        </div>
+        <div className="row justify-content-between qrcode mt-5">
+          <div className="col-sm-6">
+            <h6>Pomelo Features</h6>
+            <h3 className="mt-4"><span className="pr">Pomelo</span>Wallet</h3>
+            <img src={gql} alt="barcode" className="img-fluid w-100 mt-5" />
           </div>
           <div className="col-sm-5">
             <p>Are you tired of constantly inputting your credit cards when shopping online or paying bills? With Pomelo, you never have to do that again. Pomelo Wallet lets you hold money for all your online activities, including shopping, paying bills, and sending or receiving money across all platforms.</p>
