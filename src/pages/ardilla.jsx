6de5e5d5ce8360a4863dbe0a2ffcf9ef9e598e10
orlamee <meetmichaelakindele@gmail.com>
2023-05-03 13:50:31 +0100

    cryto

--- src/pages/ardilla.jsx
+++ src/pages/ardilla.jsx
@@ -1,4 +1,4 @@
-import ArdillaHero from "../components/ardillaHero.";
+import ArdillaHero from "../components/ardillaHero";
 import ArdillaHeader from "../components/ardillaNavbar";
 import FinancialCoaching from "../components/financialCoaching";
 import NoTop from "../components/footerwithoutTop";
--- src/pages/crypto.jsx
+++ src/pages/crypto.jsx
@@ -1,8 +1,12 @@
+import CryptoHeader from "../components/cryptoHeader";
+import CryptoHero from "../components/cryptoHero";
 import NoTop from "../components/footerwithoutTop";
 
 function Crypto() {
   return (
     <div>
+      <CryptoHeader/>
+      <CryptoHero/>
       <NoTop/>
     </div>
   );
