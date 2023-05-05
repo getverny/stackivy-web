be73e554f1c84bfe89d97f9e724c546a936797af
Olamilekan Akindele <53910087+orlamee@users.noreply.github.com>
2023-05-05 14:54:47 +0100

    Add files via upload

--- src/pages/lending.jsx
+++ src/pages/lending.jsx
@@ -1,6 +1,13 @@
+import BuyNow from "../components/buyNow";
+import EmployeeLoan from "../components/employeeLoan";
 import Footer from "../components/footer";
+import GrowBusiness from "../components/growBusiness";
 import Info from "../components/info";
+import LendingHero from "../components/leadingHero";
 import Header from "../components/navBar";
+import QuickLoan from "../components/quickLoan";
+import SalaryAdvance from "../components/salaryAdvance";
+import ServeCustomers from "../components/serveCustomers";
 
 
 
@@ -9,6 +16,13 @@ function Lending() {
   return (
     <div>
       <Header/>
+      <LendingHero/>
+      <BuyNow/>
+      <QuickLoan/>
+      <SalaryAdvance/>
+      <GrowBusiness/>
+      <ServeCustomers/>
+      <EmployeeLoan/>
       <Info/>
       <Footer/>
     </div>
