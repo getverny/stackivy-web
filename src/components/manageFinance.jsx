f20eba9cbc398209d260a96882bdad07b0e687a4
orlamee <meetmichaelakindele@gmail.com>
2023-05-08 10:16:24 +0100

    custom

--- src/components/manageFinance.jsx
+++ src/components/manageFinance.jsx
@@ -1,5 +1,6 @@
 
 import manage from "../assets/myf.svg";
+import fm from "../assets/myfs.svg";
 
 
 function ManageFinance() {
@@ -8,7 +9,7 @@ function ManageFinance() {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-sm-6 mb-3 mt-9">
-            <h2>Manage your<br/><span className="all-in-one">financial system.</span></h2>
+            <img src={fm} alt="manage your finance" className="img-fluid" />
             <h6 className="my-5">Manage your financial system with ease on Stackivy's Onescurd. Get great insights and financial tips to help navigate and control your company's finances.</h6>
           </div>
           <div className="col-sm-6">
--- src/components/strategy.jsx
+++ src/components/strategy.jsx
@@ -1,5 +1,6 @@
 
 import manage from "../assets/scg.svg";
+import fm from "../assets/fmsc.svg";
 
 
 function Strategy() {
@@ -11,7 +12,7 @@ function Strategy() {
             <img src={manage} alt="manage your finance" className="img-fluid w-100" />
           </div>
           <div className="col-sm-6 mt-9">
-            <h2><span className="all-in-one">Financial <br/>management</span> and <span className="all-in-one">strategy Consultation.</span></h2>
+            <img src={fm} alt="manage your finance" className="img-fluid" />
             <h6 className="my-5">Manage your financial system with ease on Stackivy's Onescurd. Get great insights and financial tips to help navigate and control your company's finances.</h6>
           </div>
         </div>
