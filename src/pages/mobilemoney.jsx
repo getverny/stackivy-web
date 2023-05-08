31e5ed58c43fd240cab37b23e5be58c70aed6601
orlamee <meetmichaelakindele@gmail.com>
2023-05-08 14:43:18 +0100

    meta tags

--- src/pages/mobilemoney.jsx
+++ src/pages/mobilemoney.jsx
@@ -4,12 +4,15 @@ import MobileHeader from "../components/mmHeader";
 import MobileHero from "../components/mobileHero";
 import MobileInfo from "../components/mobileInfo";
 import SendMoney from "../components/sendMoney";
+import {ReactTitle} from 'react-meta-tags';
+
 
 
 
 function MobileMoney() {
   return (
     <div>
+      <ReactTitle title="Mobile Money | Stackivy"/>
       <MobileHeader/>
       <MobileHero/>
       <SendMoney/>
--- src/pages/openBanking.jsx
+++ src/pages/openBanking.jsx
@@ -1,8 +1,18 @@
 import BudgetFooter from "../components/budgetFooter";
+import MobileHeader from "../components/mmHeader";
+// import {ReactTitle} from 'react-meta-tags';
+import MetaTags from 'react-meta-tags';
 
 function OpenBanking() {
   return (
     <div>
+      <MetaTags>
+        <title>Open Banking | Stackivy</title>
+        <meta name="description" content="Build Better and More Intimate Financial Products" />
+        <meta property="og:title" content="Open Banking" />
+      </MetaTags>
+      {/* <ReactTitle title="Open Banking | Stackivy"/> */}
+      <MobileHeader/>
       <BudgetFooter/>
     </div>
   );
