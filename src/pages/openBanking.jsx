ffee1146c8f39ad454177b241f4fbd0d370e6b04
orlamee <meetmichaelakindele@gmail.com>
2023-05-09 10:34:43 +0100

    open bank

--- src/pages/openBanking.jsx
+++ src/pages/openBanking.jsx
@@ -1,11 +1,23 @@
+import Access from "../components/access";
 import BudgetFooter from "../components/budgetFooter";
+import Documentation from "../components/documentation";
 import MobileHeader from "../components/mmHeader";
+import OpenHero from "../components/openHero";
+import SpaceInfo from "../components/spaceInfo";
+import Spaycemoon from "../components/spaycemoon";
 
 function OpenBanking() {
   return (
     <div>
       <MobileHeader/>
-      <BudgetFooter/>
+      <OpenHero/>
+      <Access/>
+      <Documentation/>
+      <Spaycemoon/>
+      <SpaceInfo/>
+      <div className="bg-black">
+        <BudgetFooter/>
+      </div>
     </div>
   );
 }
