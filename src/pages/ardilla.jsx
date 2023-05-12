3df8ca77d18d9eb4fc5dacab8b7a3e35a9f02564
orlamee <meetmichaelakindele@gmail.com>
2023-05-12 14:10:03 +0100

    fixes

--- src/pages/ardilla.jsx
+++ src/pages/ardilla.jsx
@@ -6,10 +6,15 @@ import LearnEarn from "../components/learnEarn";
 import SavePace from "../components/savePace";
 import Traction from "../components/traction";
 import Transact from "../components/transactArdilla";
+import { Title, Description } from "react-head-meta";
 
 function Ardilla() {
   return (
     <div>
+      <div>
+        <Title title="Ardilla | Stackivy" />
+        <Description description="The all in one platform for all your financial needs, whether business or personal. We are in the business of helping you achieve your financial goals" />
+      </div>
       <ArdillaHeader/>
       <ArdillaHero/>
       <FinancialCoaching/>
