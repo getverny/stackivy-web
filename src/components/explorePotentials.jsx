089924c16b0d170aa51b6b7fb307dc7edc18a138
orlamee <meetmichaelakindele@gmail.com>
2023-05-16 09:07:37 +0100

    fixes

--- src/components/explorePotentials.jsx
+++ src/components/explorePotentials.jsx
@@ -10,7 +10,6 @@ function ExplorePotential() {
           <div className="col-md-12">
             <div className="footer-top faq-foot">
               <h4 className="text-white">Explore the Potential of your Start-up<br/>with Stackivy</h4>
-              {/* <h6 className="text-white my-4 w-50">Stackivy community is Africa's leading finance forum comprising of the best financial minds on the continent. Our community is changing the narrative of finance on the continent and we need your input.</h6> */}
               <Link><img src={talk} alt="let-talk" className="img-fluid float-end" /></Link>
             </div>
           </div>
