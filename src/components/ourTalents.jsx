6b7c2cfdf88b94af840638d23025aa09802b4084
orlamee <meetmichaelakindele@gmail.com>
2023-05-18 13:06:26 +0100

    fixes

--- src/components/ourTalents.jsx
+++ src/components/ourTalents.jsx
@@ -1,4 +1,7 @@
 import careers from "../assets/human.png";
+import motivate from "../assets/bicep.png";
+import team from "../assets/team.png";
+import teach from "../assets/teach.png";
 
 
 function OurTalents() {
@@ -13,6 +16,21 @@ function OurTalents() {
             <img src={careers} alt="career" className="img-fluid w-100 animate__animated animate__slideInLeft animate__fast w-100" />
           </div>
           <div className="col-sm-5">
+            <div className="w-87">
+              <img src={motivate} alt="motivate" className="img-fluid mb-3" width={90} />
+              <h3>How Motivated Are You?</h3>
+              <p>Motivation is the bedrock of any achievement, and we want to ensure you are properly motivated to achieve excellence at Stackivy</p>
+            </div>
+            <div className="w-87 mt-5">
+              <img src={teach} alt="motivate" className="img-fluid mb-3" width={90} />
+              <h3>Are You Teachable?</h3>
+              <p>Achieving our mission means we are learning every day. We want to ensure you are teachable and eager to broaden your horizon.</p>
+            </div>
+            <div className="w-87 mt-5">
+              <img src={team} alt="motivate" className="img-fluid mb-3" width={90} />
+              <h3>Can You Adapt A New Team?</h3>
+              <p>Our chemistry is our bread and butter. We want to ensure you are compatible with our team, and the sky will be the limit.</p>
+            </div>
           </div>
         </div>
       </div>
